93ec5f98f75c175ce3ff020f96dc16f7cd8da1d7
Ashe :3 <ashe@noi.se.net>
2024-02-16 17:53:27 +0100

    fixed permissions on my site folder so no need sudo on the publish script YIPPIEE

--- publish.sh
+++ publish.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Check if the script is being run as root
-if [ "$EUID" -ne 0 ]; then
-	echo "Please run this script as root using sudo."
-	exit 1
-fi
-
 # Run Zola build in the current folder
 echo "Running Zola build..."
 zola build
